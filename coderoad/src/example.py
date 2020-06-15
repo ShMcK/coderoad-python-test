c54e88699bf106b367e03651c6f2ad67f444cc87
Beau Carnes <beaucarnes@gmail.com>
2020-06-15 16:48:23 -0400

    INIT setup

---
+++ coderoad/src/example.py
@@ -0,0 +1,16 @@
+# Solution 
+def add(*args):
+    '''Add 1 or more numbers together'''
+    sum = 0
+    for arg in args:
+        sum += arg
+    return sum
+
+def main():
+    print('hello')
+    print(str(add(1)))
+    print(str(add(1, 2)))
+    print(str(add(1, 2, 3)))
+
+if __name__ == '__main__':
+    main()
