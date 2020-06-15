beeffe8bdce05a7ddc3fdd8822581a0a4db3c43f
Beau Carnes <beaucarnes@gmail.com>
2020-06-15 16:48:23 -0400

    INIT

---
+++ src/example.py
@@ -0,0 +1,12 @@
+# Solution 
+def add(*args):
+    '''Add 1 or more numbers together'''
+    return 0
+
+def main():
+    print('add(1) = ' + str(add(1)))
+    print('add(1, 2) = ' + str(add(1, 2)))
+    print('add(1, 2, 3) = ' + str(add(1, 2, 3)))
+
+if __name__ == '__main__':
+    main()
